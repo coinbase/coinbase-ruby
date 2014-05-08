396b0e2a8b2c90ef388254049f6df4dc7fd96e56
Alex Ianus <aianus@coinbase.com>
2014-05-08 17:50:16 +0700

    OAuth client WIP

--- lib/coinbase/oauth_client.rb
+++ lib/coinbase/oauth_client.rb
@@ -3,18 +3,38 @@ require 'oauth2'
 module Coinbase
   class OAuthClient < Client
 
-    COINBASE_API_URL = 'https://coinbase.com/api/v1'
+    AUTHORIZE_URL = 'https://coinbase.com/oauth/authorize'
+    TOKEN_URL     = 'https://coinbase.com/oauth/token'
 
-    def initialize(client_id, client_secret, access_token=nil, options={})
-      @oauth_client = OAuth2::Client.new(client_id, client_secret)
-      @oauth_client.connection.url_prefix = options[:base_uri] || COINBASE_API_URL
-      token_options = {
-        :refresh_token => options[:refresh_token],
-        :expires_at => options[:expires_at],
-        :expires_in => options[:expires_in],
-        :expires => options[:expires]
+    # Initializes a Coinbase Client using OAuth 2.0 credentials
+    #
+    # @param [String] client_id this application's Coinbase OAuth2 CLIENT_ID
+    # @param [String] client_secret this application's Coinbase OAuth2 CLIENT_SECRET
+    # @param [Hash] user_credentials OAuth 2.0 credentials to use
+    # @option user_credentials [String] access_token Must pass either this or token
+    # @option user_credentials [String] token Must pass either this or access_token
+    # @option user_credentials [String] refresh_token Optional
+    # @option user_credentials [Integer] expires_at Optional
+    # @option user_credentials [Integer] expires_in Optional
+    #
+    # Please note access tokens will be automatically refreshed when expired
+    # Use the credentials method when finished with the client to retrieve up-to-date credentials
+    def initialize(client_id, client_secret, user_credentials, options={})
+      client_opts = {
+        :site          => options[:base_uri] || BASE_URI,
+        :authorize_url => options[:authorize_url] || AUTHORIZE_URL,
+        :token_url     => options[:token_url] || TOKEN_URL,
+        :ssl           => {
+                            :verify => true,
+                            :cert_store => ::Coinbase::Client.whitelisted_cert_store
+                          }
       }
-      @oauth_token = OAuth2::AccessToken.new(@oauth_client, access_token, token_options) if access_token
+      @oauth_client = OAuth2::Client.new(client_id, client_secret, client_opts)
+      token_hash = user_credentials.dup
+      token_hash[:access_token] ||= token_hash[:token]
+      token_hash.delete :expires
+      raise "No access token provided" unless token_hash[:access_token]
+      @oauth_token = OAuth2::AccessToken.from_hash(@oauth_client, token_hash)
     end
 
     def http_verb(verb, path, options={})
@@ -33,22 +53,12 @@ module Coinbase
 
     def oauth_token
       raise "Access token not initialized." unless @oauth_token
+      @oauth_token = @oauth_token.refresh! if @oauth_token.expired?
       @oauth_token
     end
 
-    def refresh!
-       @oauth_token = oauth_token.refresh!
-    end
-
-    def get_authorize_url(redirect_uri, options={})
-      scope = options[:scope] ? options[:scope].join('+') : nil
-      url = @oauth_client.auth_code.authorize_url(redirect_uri: redirect_uri, scope: scope)
-      url = url + '&' + options[:params] if options[:params]
-      url
-    end
-
-    def acquire_access_token(authorization_code, redirect_uri)
-      @oauth_token = @oauth_client.auth_code.get_token(authorization_code, redirect_uri: redirect_uri)
+    def credentials
+      oauth_token.to_hash
     end
 
     private
