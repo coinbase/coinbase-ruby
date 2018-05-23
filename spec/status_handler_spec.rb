7d7b967b20a5915091971e8c633dd9f7adc9c68d
Mufudzi Masaire <mufudzimasaire@gmail.com>
2018-05-23 22:14:15 +0100

    Add spec for Coinbase::Wallet::StatusHandler service

---
+++ spec/status_handler_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe Coinbase::Wallet::StatusHandler do
+  before :each do
+    stub_request(:get, /.*/)
+      .to_return(body: { errors: [{ id: '404', message: 'test' }] }.to_json, status: 404)
+  end
+
+  describe '.call' do
+    it 'handles the response error' do
+      # Arrange
+      uri = URI.parse("https://api.coinbase.com")
+      @conn = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new('/v2/prices/historic')
+      response = Coinbase::Wallet::NetHTTPResponse.new(@conn.request(request))
+      status_handler = Coinbase::Wallet::StatusHandler.new(response: response)
+
+      # Act & Assert
+      expect { status_handler.call }.to raise_error(Coinbase::Wallet::NotFoundError)
+    end
+  end
+
+  describe '.check_response_status' do
+    it 'handles the response error' do
+      # Arrange
+      uri = URI.parse("https://api.coinbase.com")
+      @conn = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new('/v2/accounts/primary')
+      response = Coinbase::Wallet::NetHTTPResponse.new(@conn.request(request))
+
+      # Act & Assert
+      expect {
+        Coinbase::Wallet::StatusHandler.check_response_status(response)
+      }.to raise_error(Coinbase::Wallet::NotFoundError)
+    end
+  end
+end
