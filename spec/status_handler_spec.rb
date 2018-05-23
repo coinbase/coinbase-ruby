1ea7c41ab92735ac589af5512ac9012038b4f82e
Mufudzi Masaire <mufudzimasaire@gmail.com>
2018-05-23 22:50:55 +0100

    Clean up Coinbase::Wallet::StatusHandler spec

--- spec/status_handler_spec.rb
+++ spec/status_handler_spec.rb
@@ -1,16 +1,19 @@
 require 'spec_helper'
 
 describe Coinbase::Wallet::StatusHandler do
+  before :all do
+    uri = URI.parse("https://api.coinbase.com")
+    @conn = Net::HTTP.new(uri.host, uri.port)
+  end
+
   before :each do
     stub_request(:get, /.*/)
-      .to_return(body: { errors: [{ id: '404', message: 'test' }] }.to_json, status: 404)
+      .to_return(body: { warnings: [{ message: 'test' }], errors: [{ id: '404',  message: 'test' }] }.to_json, status: 404)
   end
 
   describe '.call' do
     it 'handles the response error' do
       # Arrange
-      uri = URI.parse("https://api.coinbase.com")
-      @conn = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Get.new('/v2/prices/historic')
       response = Coinbase::Wallet::NetHTTPResponse.new(@conn.request(request))
       status_handler = Coinbase::Wallet::StatusHandler.new(response: response)
@@ -23,8 +26,6 @@ describe Coinbase::Wallet::StatusHandler do
   describe '.check_response_status' do
     it 'handles the response error' do
       # Arrange
-      uri = URI.parse("https://api.coinbase.com")
-      @conn = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Get.new('/v2/accounts/primary')
       response = Coinbase::Wallet::NetHTTPResponse.new(@conn.request(request))
 
