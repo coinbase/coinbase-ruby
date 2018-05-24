be61075085016d65e79cbd4c0cc751c236af86b7
Mufudzi Masaire <mufudzimasaire@gmail.com>
2018-05-24 21:29:43 +0100

    Add ability to pass error message when raising an exception and set default error_klass to APIError

--- lib/coinbase/wallet/status_handler.rb
+++ lib/coinbase/wallet/status_handler.rb
@@ -43,7 +43,7 @@ module Coinbase
 
       def handle_oauth_errors
         if response_status >= 400 && response_body['error']
-          raise Wallet::APIError.new(response_body['error_description'])
+          raise_exception('APIError', response_body['error_description'])
         end
       end
 
@@ -53,14 +53,14 @@ module Coinbase
           when 400..401 then raise_exception(Wallet::CLIENT_ERRORS[response_errors.first['id']] || Wallet::CLIENT_ERRORS[response_status])
           when 400..499 then raise_exception(Wallet::CLIENT_ERRORS[response_status])
           when 500..599 then raise_exception(Wallet::SERVER_ERRORS[response_status])
-          else raise Wallet::APIError.new("[#{response_status}] #{response_body}")
+          else raise_exception
           end
         end
       end
 
       def handle_regular_errors
         if response_status > 400
-          raise Wallet::APIError.new("[#{response_status}] #{response_body}")
+          raise_exception
         end
       end
 
@@ -72,8 +72,8 @@ module Coinbase
         message
       end
 
-      def raise_exception(error_klass)
-        raise Wallet.const_get(error_klass), error_message
+      def raise_exception(error_klass = 'APIError', message = error_message)
+        raise Wallet.const_get(error_klass), "[#{response_status}] #{message}"
       end
     end
   end
